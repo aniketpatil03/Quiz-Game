cc90cda01fdd7148734d5fccc54779c5dd2fa86f
Aniket Patil <55218425+aniketpatil03@users.noreply.github.com>
2021-07-11 15:11:50 +0530

    Add files via upload

---
+++ question_model.py
@@ -0,0 +1,9 @@
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.correct_answer = q_answer
+
+
+question1 = Question("First question", "First answer")
+print(question1.text)
+print(question1.correct_answer)
